0274a874b0d74950e0febc6c022885e99a5e8867
Nathaniel Mishkin <mishkin@aya.yale.edu>
2013-07-19 07:37:33 -0400

    First semi-complete version.

--- src/main/java/MusicFileSync.java
+++ src/main/java/MusicFileSync.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -14,25 +15,56 @@ import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Pattern;
 
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.AudioHeader;
+import org.jaudiotagger.audio.mp3.MP3AudioHeader;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagField;
+import org.jaudiotagger.tag.TagTextField;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
 
 public class MusicFileSync {
+    enum Side {
+        SOURCE,
+        DESTINATION
+    }
+    private static final String FFMPEG = "c:/Users/mishkin/ffmpeg/bin/ffmpeg.exe";
+    
     @Argument
     private List<String> arguments = new ArrayList<String>();
+    private String dstRoot;
     
     public static void main(String[] args) throws IOException {
         new MusicFileSync().doMain(args);
+    }
+    
+    
+    @SuppressWarnings("unused")
+    private static void showFileTagFields(Tag tag) {
+        Iterator<TagField> fields = tag.getFields();
+        while (fields.hasNext()) {
+            TagField field = fields.next();
+            final String value;
+            if (field instanceof TagTextField) {
+                TagTextField textField = (TagTextField) field;
+                value = textField.getContent();
+            } else {
+                value = "XXX";
+            }
+            System.out.format("  <%s> <%s>\n", field.getId(), value);
+            
+        }
     }
     
     public void doMain(String[] args) throws IOException {
@@ -47,25 +79,83 @@ public class MusicFileSync {
             return;
         }
         
+        dstRoot = arguments.get(0);
         
         Map<String, Path> dstMap = new HashMap<String, Path>();
         Path dstDatPath = fs.getPath("dst.dat");
-        fillMap(dstMap, 0, dstDatPath);
+        fillMap(Side.DESTINATION, dstMap, 0, dstDatPath);
         writeMap(dstMap, dstDatPath);
         
         Map<String, Path> srcMap = new HashMap<String, Path>();
         Path srcDatPath = fs.getPath("src.dat");
         for (int i = 1; i < arguments.size(); i++) {
-            fillMap(srcMap, i, srcDatPath);
+            fillMap(Side.SOURCE, srcMap, i, srcDatPath);
         }
         writeMap(srcMap, srcDatPath);
         
+        for (Entry<String, Path> srcEntry : srcMap.entrySet()) {
+            String srcKey = srcEntry.getKey();
+            Path dstPath = dstMap.get(srcKey);
+            Path srcPath = srcEntry.getValue();
+            if (dstPath == null) {
+                System.out.format("No destination entry for source entry: %s\n", srcKey);
+                copyFile(srcPath);
+            } else {
+                updateFile(dstPath, srcPath);
+                dstMap.remove(srcKey);
+            }
+            
+        }
+    }
 
+    private void updateFile(Path dstPath, Path srcPath) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    private void copyFile(Path srcPath) {
+        try {
+            String dstPath = String.format("%s/store/%s", dstRoot, relativize(srcPath));
+            new File(dstPath).getParentFile().mkdirs();
+            System.out.format("%s -> %s\n", srcPath, dstPath);
+            String[] command = {FFMPEG, "-y", "-loglevel", "quiet", "-i", srcPath.toString(), "-id3v2_version", "3", dstPath.toString()};
+            Process process = Runtime.getRuntime().exec(command);
+            int status = process.waitFor();
+            if (status != 0) {
+                System.err.format("ffmpeg returned status %s\n", status);
+            }
+        } catch (Exception e) {
+            System.err.format("Error copying file: %s\n", e);
+            e.printStackTrace();
+        }
+        
+    }
+
+    private Path relativize(Path srcPath) {
+        for (int i = 1; i < arguments.size(); i++) {
+            Path relPath = fs.getPath(arguments.get(i)).relativize(srcPath);
+            if (relPath.iterator().hasNext()) {
+                return relPath;
+            }
+        }
+        throw new RuntimeException("Can't relativize: " + srcPath);
+    }
+
+
+    private String makeNameFromTags(Path path) throws Exception {
+        AudioFile file = AudioFileIO.read(path.toFile());
+        Tag tag = file.getTag();        
+        TagField track = tag.getFirstField(FieldKey.TRACK);
+        return String.format("%s - %s - %s", 
+                tag.getFirstField(FieldKey.ALBUM), 
+                track, 
+                tag.getFirstField(FieldKey.TITLE)).replaceAll("[^ \\-0-9A-z]", "_"); 
     }
 
     private static FileSystem fs = FileSystems.getDefault();
     
-    private void fillMap(Map<String, Path> map, int i, Path datPath) throws IOException {
+    private void fillMap(Side side, Map<String, Path> map, int i, Path datPath) throws IOException {
         if (Files.exists(datPath)) {
             System.out.format("Reading from file %s\n", datPath);
             BufferedReader r = new BufferedReader(new FileReader(datPath.toFile()));
@@ -82,7 +172,7 @@ public class MusicFileSync {
             String dir = arguments.get(i);
             Path dirPath = fs.getPath(dir);
             System.out.format("Walking %s\n", dirPath);
-            Files.walkFileTree(dirPath, new MyFileVisitor(dirPath, map));
+            Files.walkFileTree(dirPath, new MyFileVisitor(side, dirPath, map));
         }
     }
 
@@ -101,8 +191,10 @@ public class MusicFileSync {
     private static class MyFileVisitor implements FileVisitor<Path> {
         private Map<String, Path> map;
         private Path baseDir;
+        private Side side;
 
-        MyFileVisitor(Path baseDir, Map<String, Path> map) {
+        MyFileVisitor(Side side, Path baseDir, Map<String, Path> map) {
+            this.side = side;
             this.map = map;
             this.baseDir = baseDir;
         }
@@ -115,11 +207,12 @@ public class MusicFileSync {
             return FileVisitResult.CONTINUE;
         }
 
+        private static final Pattern SUFFIX_PATTERN = Pattern.compile("(.+(\\.(?i)(mp3|m4a))$)");
+        
         public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
             String pathString = path.toString();
             
-            int length = pathString.length();
-            if (! pathString.endsWith(".mp3") && ! pathString.substring(length - 4, length - 1).equals(".m4")) {
+            if (! SUFFIX_PATTERN.matcher(pathString).matches()) {
                 return FileVisitResult.CONTINUE;
             }
             
@@ -127,10 +220,29 @@ public class MusicFileSync {
             try {
                 file = AudioFileIO.read(path.toFile());
                 Tag tag = file.getTag();
-                String key = String.format("%s|%s|%s", tag.getFirst(FieldKey.ALBUM), tag.getFirst(FieldKey.TRACK), tag.getFirst(FieldKey.TITLE));
-                map.put(key, baseDir.relativize(path));
+//                showFileTagFields(tag);
+                
+                if (side == Side.SOURCE) {
+                    String genre = tag.getFirst(FieldKey.GENRE);
+                    if (genre.equalsIgnoreCase("Podcast")) {
+                        return FileVisitResult.CONTINUE;
+                    }
+                }
+                
+                
+                AudioHeader audioHeader = file.getAudioHeader();
+                
+                double trackLength = audioHeader instanceof MP3AudioHeader ? 
+                        ((MP3AudioHeader) audioHeader).getPreciseTrackLength() : audioHeader.getTrackLength();
+//                String key = String.format("%s|%s|%s|%f", tag.getFirst(FieldKey.ALBUM), tag.getFirst(FieldKey.TRACK), tag.getFirst(FieldKey.TITLE), trackLength);
+                String key = String.format("%s|%s|%f", tag.getFirst(FieldKey.ALBUM), tag.getFirst(FieldKey.TRACK), trackLength);
+//                Path relPath = baseDir.getParent().relativize(path);
+                Path prevPath = map.put(key, path);
+                if (prevPath != null) {
+                    System.err.format("Duplicate for key <%s>:\n  %s\n  %s\n", key, path, prevPath); 
+                }
             } catch (Exception e) {
-                System.err.format("Error with file: %s\n", path);
+                System.err.format("Error with file: %s (%s)\n", path, e.getMessage());
 //                e.printStackTrace();
             }
             return FileVisitResult.CONTINUE;
